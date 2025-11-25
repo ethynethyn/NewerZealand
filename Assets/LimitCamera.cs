e78abfaa5de57b9920a1c6d68e7388174722dfea
ethynethyn <131282926+ethynethyn@users.noreply.github.com>
2025-11-26 01:23:36 +1300

    MiniMap

---
+++ Assets/LimitCamera.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class LimitCamera : MonoBehaviour
+{
+
+    public GameObject Player;
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(Player.transform.position.x, 40, Player.transform.position.z);
+    }
+}
